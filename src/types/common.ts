52bda8020e2bcac4d06467f27c9ea1914e36f195
ceuk <ceuk.dev@gmail.com>
2021-01-27 21:14:29 +0000

    clean up types

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,1 @@
-export type AnyUnaryFn = (a: any) => any
-export type MapCallback<T = any, U = any> = ($value: T) => U
-export type PartialMapCallback<T = any, U = any> = ($value?: T) => U
-export type ArrayCallbackFn<T = any, U = any> = (v: T, index?: number, array?: T[]) => U
+export type MapCallback<T = never, U = never> = ($value: T) => U
